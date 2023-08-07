044da8816e5b56ffaa2596e43efe910484864ebc
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-08-07 12:59:21 +0500

    rename weth contract

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -1,5 +1,4 @@
 import pytest
-from test_token import amountToBuyMark, ownerAndFactory, testToken
 from scripts.deployPool import deployLiquidityPool
 from scripts.scriptsPool import (
     createDeposit,
@@ -9,57 +8,63 @@ from scripts.scriptsPool import (
     buyTokens,
     approve
 )
+from test_token import (
+    amountToBuyMark,
+    ownerAndFactories,
+    testToken,
+    wethToken
+)
 
-# @pytest.mark.parametrize('amountToBuy', amountToBuyMark)
-# def test_deployPool(ownerAndFactory, amountToBuy):
-#     owner, myToken = ownerAndFactory
-#     buyTokens(owner, myToken.address, amountToBuy)
-#     pool = deployLiquidityPool(owner, owner, myToken.token())
+@pytest.mark.parametrize('amountToBuy', amountToBuyMark)
+def test_deployPool(ownerAndFactories, amountToBuy):
+    owner, myToken, weth = ownerAndFactories
+    buyTokens(owner, myToken.address, amountToBuy)
+    pool = deployLiquidityPool(owner, weth.token(), myToken.token())
 
-#     assert str(pool.address) != '0'
+    assert str(pool.address) != '0'
 
-# @pytest.mark.parametrize(
-#         'amountToBuy, deposit',
-#         [pytest.param((0, 0), "Your don't have tokens :(", marks=pytest.mark.xfail), pytest.param((100, 120), "Transfer amount exceeds balance!", marks=pytest.mark.xfail), (100, 50)]
-# )
-# def test_createDeposit(ownerAndFactory, testToken, amountToBuy, deposit):
-#     owner, myToken = ownerAndFactory
-#     buyTokens(owner, myToken.address, amountToBuy)
-#     pool = deployLiquidityPool(owner, owner, myToken.token())
-#     ownerTokenBalance = testToken.balanceOf(owner)
-#     poolBalance = testToken.balanceOf(pool.address)
+@pytest.mark.parametrize(
+        'amountToBuy, deposit',
+        [pytest.param((0, 0), "Your don't have tokens :(", marks=pytest.mark.xfail), pytest.param((100, 120), "Transfer amount exceeds balance!", marks=pytest.mark.xfail), (100, 50)]
+)
+def test_createDeposit(ownerAndFactories, testToken, amountToBuy, deposit):
+    owner, myToken, weth = ownerAndFactories
+    buyTokens(owner, myToken.address, amountToBuy)
+    pool = deployLiquidityPool(owner, weth.token(), myToken.token())
+    ownerTokenBalance = testToken.balanceOf(owner)
+    poolBalance = testToken.balanceOf(pool.address)
 
-#     approve(testToken, pool.address, deposit, owner)
-#     createDeposit(owner, myToken.token(), deposit)
+    approve(testToken, pool.address, deposit, owner)
+    createDeposit(owner, myToken.token(), deposit)
 
-#     assert testToken.balanceOf(owner) == ownerTokenBalance - deposit
-#     assert testToken.balanceOf(pool.address) == poolBalance + deposit
-
-# @pytest.mark.parametrize('deposit', amountToBuyMark)
-# def test_withdraw(ownerAndFactory, testToken, deposit):
-#     owner, myToken = ownerAndFactory
-#     buyTokens(owner, myToken.address, deposit)
-#     pool = deployLiquidityPool(owner, owner, myToken.token())
-#     approve(testToken, pool.address, deposit, owner)
-#     createDeposit(owner, myToken.token(), deposit)
-
-#     ownerTokenBalance = testToken.balanceOf(owner)
-#     poolBalance = testToken.balanceOf(pool.address)
-#     withdraw(owner, myToken.token(), deposit)
-
-#     assert testToken.balanceOf(owner) == ownerTokenBalance + deposit
-#     assert testToken.balanceOf(pool.address) == poolBalance - deposit
-
+    assert testToken.balanceOf(owner) == ownerTokenBalance - deposit
+    assert testToken.balanceOf(pool.address) == poolBalance + deposit
 
 @pytest.mark.parametrize('deposit', amountToBuyMark)
-def test_exchange(ownerAndFactory, testToken, deposit, amount=10):
-    owner, myToken = ownerAndFactory
+def test_withdraw(ownerAndFactories, testToken, deposit):
+    owner, myToken, weth = ownerAndFactories
     buyTokens(owner, myToken.address, deposit)
-    pool = deployLiquidityPool(owner, owner, myToken.token())
+    pool = deployLiquidityPool(owner, weth.token(), myToken.token())
+    approve(testToken, pool.address, deposit, owner)
+    createDeposit(owner, myToken.token(), deposit)
 
-    buyTokens(owner, owner, deposit*2)
+    ownerTokenBalance = testToken.balanceOf(owner)
+    poolBalance = testToken.balanceOf(pool.address)
+    withdraw(owner, myToken.token(), deposit)
 
-    approve(testToken, pool.address, amount, owner)
-    print(f'ToTokenBalance: {owner.balanceOf(pool.address)}')
-    exchange(owner, testToken, owner, amount)
+    assert testToken.balanceOf(owner) == ownerTokenBalance + deposit
+    assert testToken.balanceOf(pool.address) == poolBalance - deposit
+
+
+# @pytest.mark.parametrize('deposit', amountToBuyMark)
+# def test_exchange(ownerAndFactories, testToken, deposit, amount=10):
+#     owner, myToken = ownerAndFactories
+#     buyTokens(owner, myToken.address, deposit)
+#     pool = deployLiquidityPool(owner, owner, myToken.token())
+
+#     buyTokens(owner, owner, deposit*2)
+
+#     approve(testToken, pool.address, amount, owner)
+#     print(f'ToTokenBalance: {owner.balanceOf(pool.address)}')
+#     exchange(owner, testToken, owner, amount)
     
--- tests/test_token.py
+++ tests/test_token.py
@@ -1,23 +1,29 @@
 import pytest
-from brownie import accounts, TestToken
+from brownie import accounts, TestToken, WrappedETH
 from scripts.scriptsPool import buyTokens, approve
 from scripts.deployToken import deployToken
+from scripts.deployWETH import deployWETH
 
 amountToBuyMark = [pytest.param(0, marks=pytest.mark.xfail), 10, 1000, pytest.param(100000, marks=pytest.mark.xfail)]
 
 @pytest.fixture()
-def ownerAndFactory():
+def ownerAndFactories():
     owner = accounts[0]
-    return owner, deployToken(owner)
+    return owner, deployToken(owner), deployWETH(owner)
 
 @pytest.fixture()
-def testToken(ownerAndFactory):
-    _, myToken = ownerAndFactory
+def testToken(ownerAndFactories):
+    _, myToken, _ = ownerAndFactories
     return TestToken.at(myToken.token())
 
+@pytest.fixture()
+def wethToken(ownerAndFactories):
+    _, _, weth = ownerAndFactories
+    return WrappedETH.at(weth.token())
+
 @pytest.mark.parametrize('amountToBuy', amountToBuyMark)
-def test_buyingToken(ownerAndFactory, testToken, amountToBuy):
-    owner, myToken = ownerAndFactory
+def test_buyingToken(ownerAndFactories, testToken, amountToBuy):
+    owner, myToken = ownerAndFactories
     ownerBalance = owner.balance()
 
     buyTokens(owner, myToken.address, amountToBuy)
@@ -29,8 +35,8 @@ def test_buyingToken(ownerAndFactory, testToken, amountToBuy):
     assert owner.balance() <= ownerBalance
 
 @pytest.mark.parametrize('amountToBuy', amountToBuyMark)
-def test_sellingToken(ownerAndFactory, testToken, amountToBuy, amountToSell=9):
-    owner, myToken = ownerAndFactory
+def test_sellingToken(ownerAndFactories, testToken, amountToBuy, amountToSell=9):
+    owner, myToken = ownerAndFactories
     buyTokens(owner, myToken.address, amountToBuy)
     
     approve(testToken, myToken.address, amountToSell, owner)
